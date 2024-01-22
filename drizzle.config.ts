1d03b7b4d722ab747ce8792d82af2a715b216e57
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-01-23 00:28:05 +0530

    setting up the environment for the notion-clone FIRST COMMIT

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+//Setting up the Drizzle
+
+import type { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config({path: '.env'});
+
+if(!process.env.DATABASE_URL){
+    console.log("Database Url not found!!");
+}
+
+export default {
+    schema:'./src/lib/supabase/schema.ts',
+    out:'./migrations',
+    driver:'pg',
+    dbCredentials:{
+        connectionString:process.env.DATABASE_URL || '',
+    },
+} satisfies Config;
