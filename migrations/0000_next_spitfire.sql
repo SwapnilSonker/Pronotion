f1e3d57d001c9dbdcd5e343bb38fe865a57bc981
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-01-24 00:45:27 +0530

    drizzle orm is setted up and tables are created - next is homepage

--- migrations/0000_next_spitfire.sql
+++ migrations/0000_next_spitfire.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "workspace" (
+CREATE TABLE IF NOT EXISTS "workspaces" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"created_at" timestamp with time zone,
 	"workspace_owner" uuid NOT NULL,
