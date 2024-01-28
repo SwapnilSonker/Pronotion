c5917cb69bde662aebc7b25465779db6414fc40a
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-01-28 17:16:24 +0530

    Navigation Bar Done ✅

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -64,7 +64,7 @@ const LoginPage = () => {
           disabled={isLoading}
           control={form.control}
           name="email"
-          render={(field) => (
+          render={({field}) => (
             <FormItem>
               <FormControl>
                 <Input type="email" placeholder="Email" {...field} />
@@ -77,7 +77,7 @@ const LoginPage = () => {
           disabled={isLoading}
           control={form.control}
           name="password"
-          render={(field) => (
+          render={({field}) => (
             <FormItem>
               <FormControl>
                 <Input type="password" placeholder="Password" {...field} />
